800733e52d7b1b3c5d23fb5353a894c846af511e
charity faith <charity.sotero@gmail.com>
2019-10-12 18:46:25 -0700

    create functions to take in city lat/lng and keyword of choice. map out the points.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Rank optimal apartments in an area by their surrounding resources.',
+    author='Charity Faith Sotero',
+    license='MIT',
+)
